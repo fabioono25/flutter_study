93afc5e0e7b8bd5833b747b9b1472fb4a6270977
ono <fabioono25@gmail.com>
2019-12-01 12:29:37 +0000

    Styling

--- first_app/lib/question.dart
+++ first_app/lib/question.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-
   final String questionText; //this value will never change after initialization
 
   Question(this.questionText);
 
   @override
   Widget build(BuildContext context) {
-    return Text(this.questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
+
+    // return Center(
+    //   child: Text(
+    //     questionText,
+    //     style: TextStyle(fontSize: 28),
+    //     textAlign: TextAlign.center,
+    //   ),
+    // );
   }
 }
