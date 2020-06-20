92ca447bf4ed604031064571ba50303edf1b1c5d
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-06-20 22:02:39 +0100

    returning a list of transactions

--- personal_expense/lib/main.dart
+++ personal_expense/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -13,15 +15,55 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 34.54,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Groceries',
+      amount: 12.12,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Center(
-        child: Text('Widget Playground!'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART!'),
+                // child: Container(
+                //   child: Text('CHART!'),
+                //   width: double.infinity,
+                // ),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: transactions
+                  .map((tx) => Card(
+                        child: Text(tx.title),
+                      ))
+                  .toList(),
+            )
+            // Card(
+            //   color: Colors.yellow,
+            //   child: Text('LIST OF TX'),
+            // )
+          ],
+        ));
   }
 }
